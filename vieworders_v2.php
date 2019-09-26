799ffaae52972fc0f4216667f56d1515fa5c1f6e
Mustak Ahmed <musmak224@gmail.com>
2019-09-25 23:44:41 -0400

    changed it to make it work with db

--- vieworders_v2.php
+++ vieworders_v2.php
@@ -6,35 +6,48 @@
 <html>
   <head>
     <title>Bob's Auto Parts - Customer Orders</title>
+    <style>
+      table {
+        border-collapse: collapse;
+      }
+
+      table, th, td {
+        border: 1px solid black;
+      }
+    </style>
   </head>
   <body>
     <h1>Bob's Auto Parts</h1>
     <h2>Customer Orders</h2> 
     <?php
-      @$fp = fopen("$document_root/../orders/orders.txt", 'rb');
-      flock($fp, LOCK_SH); // lock file for reading
+      $servername = "localhost";
+      $username = "mahmed29";
+      $password = "";
+      $dbname = "mahmed29";
 
-      if (!$fp) {
-        echo "<p><strong>No orders pending.<br />
-              Please try again later.</strong></p>";
-        exit;
+      // Create connection
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      // Check connection
+      if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error));
       }
 
-      while (!feof($fp)) {
-         $order= fgets($fp);
-         echo htmlspecialchars($order)."<br />";
+      $sql = "SELECT OrderId, TiresQuantity, OilQuantity, SparkPlugQuantity, Address FROM Orders";
+    
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+        echo "<table><tr><th>Order Id</th><th>Tire Quantity</th><th>Oil Quantity</th><th>Spark Quantity</th><th>Address</th></tr>";
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["OrderId"]."</td><td>".$row["TiresQuantity"]."</td><td>".$row["OilQuantity"]."</td><td>".$row["SparkPlugQuantity"]."</td><td>".$row["Address"]."</td></tr>";
       }
-
-      flock($fp, LOCK_UN); // release read lock
-
-      echo 'Final position of the file pointer is '.(ftell($fp));
-      echo '<br />';
-      rewind($fp);
-      echo 'After rewind, the position is '.(ftell($fp));
-      echo '<br />';
-
-      fclose($fp); 
-
+        echo "</table>";
+      } else {
+        echo "0 results";
+      }
+      $conn->close(); 
     ?>
+    <a href="./index.html">New Order</a>
   </body>
 </html>
